9a475985cd29863053883156abddac07b3305b8f
Hadley Wickham <hadley@rstudio.com>
2014-03-27 16:27:00 +0000

    version 0.1

---
+++ R/code.R
@@ -0,0 +1,100 @@
+#' Check if RStudio is running.
+#' 
+#' @return \code{isAvailable} a boolean; \code{verifyAvailable} an error message
+#'   if Rstudio is not running
+#' @param version_needed An optional version specification. If supplied, 
+#'   ensures that Rstudio is at least that version.
+#' @export
+#' @examples
+#' rstudioapi::isAvailable()
+#' \dontrun{rstudioapi::verifyAvailable()}
+isAvailable <- function(version_needed = NULL) {
+  identical(.Platform$GUI, "RStudio") && version_ok(version_needed)
+}
+
+version_ok <- function(version = NULL) {
+  if (is.null(version)) return(TRUE)
+  
+  getVersion() >= version
+}
+
+#' @rdname isAvailable
+#' @export
+verifyAvailable <- function(version_needed = NULL) {
+  if (!isAvailable()) stop("RStudio not running", call. = FALSE)
+  if (!version_ok(version_needed)) {
+    stop("Need at least version ", version_needed, " of RStudio. ", 
+      "Currently running ", getVersion(), call. = FALSE)
+  }
+  invisible(TRUE)  
+}
+
+#' Return the current version of the RStudio api.
+#' 
+#' @return A \code{\link{numeric_version}} which you can compare to a string
+#'   and get correct results.
+#' @export
+#' @examples
+#' \dontrun{
+#' if (rstudio::getVersion() < "0.98.100") {
+#'   message("Your version of Rstudio is quite old")
+#' }
+#' }
+getVersion <- function() {
+  verifyAvailable()
+  packageVersion("rstudio")
+}
+
+#' Call an Rstudio API function
+#' 
+#' This function will return an error if Rstudio is not running, or the 
+#' function is not available. If you want to fall back to different 
+#' behavour, use \code{\link{hasFun}}.
+#' 
+#' @param fname name of the Rstudio function to call. 
+#'   See \code{help(package = "Rstudio")}. For a complete list of functions
+#'   in the current API.
+#' @param ... Other arguments passed on to the function
+#' @export
+#' @examples
+#' if (rstudioapi::isAvailable()) {
+#'   rstudioapi::callFun("versionInfo")
+#' }
+callFun <- function(fname, ...) {
+  verifyAvailable()
+  
+  if (!exists(fname, envir = asNamespace("rstudio"), mode = "function")) {
+    stop("Function ", fname, " not found in Rstudio", call. = FALSE)
+  }
+  
+  f <- findFun(fname, mode = "function")
+  f(...)
+}
+
+#' Exists/get for Rstudio functions
+#' 
+#' These are specialised versions of \code{\link[base]{get}} and 
+#' \code{\link[base]{exists}} that look in the rstudio package namespace. 
+#' If Rstudio is not running,  \code{exists} will return \code{FALSE}, 
+#' and \code{getRstudio} will raise an error.
+#' 
+#' @param name name of object to look for
+#' @param ... other arguments passed on to \code{\link[base]{exists}} and 
+#'   \code{\link[base]{get}}
+#' @param version_needed An optional version specification. If supplied, 
+#'   ensures that Rstudio is at least that version. This is useful if 
+#'   function behaviour has changed over time.
+#' @export
+#' @examples
+#' rstudioapi::hasFun("viewer")
+hasFun <- function(name, version_needed = NULL, ...) {
+  if (!isAvailable(version_needed)) return(FALSE)
+  exists(name, envir = asNamespace("rstudio"), ...)
+}
+
+#' @export
+#' @rdname hasFun
+findFun <- function(name, version_needed = NULL, ...) {
+  verifyAvailable(version_needed)
+  get(name, envir = asNamespace("rstudio"), ...)
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+# rstudioapi
+
+[![Build Status](https://travis-ci.org/rstudio/rstudioapi.png?branch=master)](https://travis-ci.org/rstudio/rstudioapi)
+
+The rstudioapi package is designed to make it easy to conditionally access the Rstudio API from CRAN packages, avoiding any potential problems with `R CMD check`. This package contains a handful of useful wrapper functions to access the API. To see the functions that are currently available in the API, run `help(package = "rstudio")`
+
+# Example uses
+
+```R
+# rstudioapi is designed to never be attached to your search path.
+# Always prefix function calls with rstudioapi::
+
+# Returns T/F
+rstudioapi::isAvailable()
+# Returns error if not available
+rstudioapi::verifyAvailable()
+
+# Optional argument allows you to specify version requirement
+rstudioapi::isAvailable("0.99")
+rstudioapi::verifyAvailable("0.99")
+
+# Call an rstudio function
+rstudioapi::callFun("viewer", "http://localhost:8080")
+
+# This will raise an error if rstudio is not running, or the function
+# is not found. To run a different function if it's not available,
+# use exists
+if (rstudioapi::hasFun("viewer")) {
+  rstudioapi::callFun("viewer", "http://localhost:8080")
+} else {
+  browseURL("http://localhost:8080")
+}
+
+# You can use find to get the function. Throws an error if the function
+# does not exist.
+rstudioapi::findFun("viewer")
+
+# You can also check version in exists and find
+rstudioapi::findFun("viewer", 0.99)
+rstudioapi::hasFun("viewer", 0.99)
+```
+
+# Installation
+
+* Install the development version with `devtool::install_github("rstudio/rstudioapi")
+`
